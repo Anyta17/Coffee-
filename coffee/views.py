4da7126be55db4e1ded8d95201e5dd46adaea564
Anna Blahitka <anettaabl@gmail.com>
2024-02-14 15:08:53 +0200

    Refactor the project

--- coffee/views.py
+++ coffee/views.py
@@ -1,23 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from django.shortcuts import redirect
-from django.utils import translation
 
 from coffee.models import Coffee
 
 
-def switch_language(request, language_code):
-    translation.activate(language_code)
-    request.session[translation.LANGUAGE_SESSION_KEY] = language_code
-    return redirect(request.META.get("HTTP_REFERER", "/"))
-
-
 def index(request, language_code=None):
     recipes = Coffee.objects.all()
+    all_recipes = Coffee.objects.count()
 
     context = {
         "recipes": recipes,
         "language_code": language_code,
+        "all_recipes": all_recipes,
     }
 
     return render(request, "coffee/index.html", context=context)
